19aac42a4bee8c224c19cbc9678cbde50b6d1c5d
EreminaOlga <95230471+EreminaOlga@users.noreply.github.com>
2022-01-07 23:38:03 +0300

    Привет. Bank project creation

--- src/MyBankApp.java
+++ src/MyBankApp.java
@@ -60,6 +60,11 @@ public class MyBankApp {
             System.out.println(clientsInBank);
         } while (clientsInBank < 20);
 
+        for (int window = 0; window < 4; window++){
+            for (int clientsInLine = 0; clientsInLine < 16; clientsInLine++)
+            System.out.println("Окно: " + window + ", номер в очереди: " + clientsInLine);
+        }
+
 
     }
 
